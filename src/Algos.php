2a25f33c4325ee69deeff6e6a79a2afc3681a526
= <=>
2025-09-24 13:46:11 -0400

    Premiere version de Algos.php et produits.php

--- src/Algos.php
+++ src/Algos.php
@@ -1,6 +1,7 @@
 <!-- Fonction pour créer des posts -->
 
 <?php 
+
 function creerPoste($titre, $description, $prix, $negociable, $image, $vendeur){
     echo
         '<div class="bg-anthracite rounded-xl shadow hover:shadow-xl transition overflow-hidden">
@@ -8,8 +9,8 @@ function creerPoste($titre, $description, $prix, $negociable, $image, $vendeur){
             <div class="p-5">
                 <h3 class="font-semibold text-lg text-light">' . $titre . '</h3>
                 <p class="text-light text-sm mt-1">' . $description . '</p>
-                <p class="text-silver font-bold mt-3">' . $prix . ($negociable ? ' (Négociable)' : '(Non-négociable') . '</p>
-                <button class="mt-4 w-full bg-silver text-blacklux py-2 rounded hover:bg-anthracite hover:text-light transition">Acheter</button>
+                <p class="text-silver font-bold mt-3">' . $prix . ($negociable === "oui" ? ' (Négociable)' : '(Non-négociable') . '</p>
+                <button class="mt-4 w-full bg-silver text-blacklux py-2 rounded hover:bg-anthracite hover:text-light transition">Contacter ' . $vendeur . '</button>
             </div>
-        </div>'
-}
+        </div>';
+};
--- src/include/produits.php
+++ src/include/produits.php
@@ -1,8 +1,36 @@
   <!-- Produits -->
+  <section>
+    <h2 class="text-xl font-semibold mb-8 text-anthracite">Produits mis en avant</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    
+    <?php
+    include_once "Algos.php";
 
-  <h2 class="text-xl font-semibold mb-8 text-anthracite">Produits mis en avant</h2>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    $chemin = "../fichiers_information/annonces.csv";
+    if(file_exists($chemin) && fopen($chemin, "r") !== false)
+        $fich = fopen($chemin, "r");
+
+    $titre = $description = $prix = $negociable = $image = $vendeur = "";
+
+    $delimiteur = "|";
+    $maxLongueur = 1000;
+
+    while(!feof($fich)){
+        $ligne = fgetcsv($fich, $maxLongueur, $delimiteur, "\"", "\\");
+        if($ligne !== false && count($ligne) === 6){
+            $titre = $ligne[0];
+            $description = $ligne[1];
+            $prix = $ligne[2];
+            $negociable = $ligne[3];
+            $image = $ligne[4];
+            $vendeur = $ligne[5];
+        }
+
+        creerPoste($titre, $description, $prix, $negociable, $image, $vendeur);
+    }
 
 
+    ?>
 
-  </div>
+    </div>
+  </section>
