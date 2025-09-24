09310e023a7baa647382cbf6bef5bfad879527d3
= <=>
2025-09-24 13:13:02 -0400

    first commit

---
+++ src/include/produits.php
@@ -0,0 +1,8 @@
+  <!-- Produits -->
+
+  <h2 class="text-xl font-semibold mb-8 text-anthracite">Produits mis en avant</h2>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+
+
+
+  </div>
